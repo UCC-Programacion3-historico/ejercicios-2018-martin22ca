cc43cd51bfc5a8ff43e18f7ed931bb42c46f4df8
martin <caceresmartin22@gmail.com>
2019-08-07 11:00:38 -0300

    1.1

--- U00_Repaso/Ej-02/Calculadora.h
+++ U00_Repaso/Ej-02/Calculadora.h
@@ -2,9 +2,29 @@
 #define REPASO_CALCULADORA_H
 
 
+template<class T, class P>
 class Calculadora {
+public:
+    T suma(T a, T b);
 
+    T resta(T a, T b);
+
+    T multiplicacion(T a, T b);
+
+    T division(T a, T b);
 };
+
+template<class T, class P>
+T Calculadora<T, P>::suma(T a, T b) { return a + b; }
+
+template<class T, class P>
+T Calculadora<T, P>::resta(T a, T b) { return a - b; }
+
+template<class T, class P>
+T Calculadora<T, P>::multiplicacion(T a, T b) { return a * b; }
+
+template<class T, class P>
+T Calculadora<T, P>::division(T a, T b) { return (P) a / b; }
 
 
 #endif //REPASO_CALCULADORA_H
--- U00_Repaso/Ej-02/main.cpp
+++ U00_Repaso/Ej-02/main.cpp
@@ -5,5 +5,8 @@ using namespace std;
 
 int main() {
     cout << "Ejercicio 00/02\n" << endl;
+    Calculadora <int, float> Mica;
+    cout<<"El resultador es:";
+    cout<<Mica.division(10,3)<<endl;
     return 0;
 }
